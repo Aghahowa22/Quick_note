ca6023ee8f78306de85ab37e185f3ac497361d7d
Aghahowa Edosa <raymondedosa400@gmail.com>
2025-05-19 03:45:58 -0700

    Added navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 
+import Navbar from "./component/Navbar";
 import Landing from "./routes/Landing";
 import Login from "./routes/Login";
 import Signup from "./routes/Signup";
@@ -7,11 +8,14 @@ import { Routes, Route, Navigate } from "react-router-dom";
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+      <div className="min-h-screen bg-slate-50">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
     </>
   );
 }
